6afd213181b3eadc66f521b312138d79ffc23d94
ldex <laurent.duveau+git@gmail.com>
2023-09-18 16:26:25 -0400

    Fin du jour 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ProductListComponent]
 })
 export class AppModule { }
---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/models/product.interface';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  title: string = 'Products';
+  products: Product[];
+  products$: Observable<Product[]>;
+  selectedProduct: Product;
+
+  onSelect(product: Product) {
+    this.selectedProduct = product;
+  }
+
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.products$;
+
+    // this
+    //   .productService
+    //   .products$
+    //   .subscribe(
+    //     data => this.products = data
+    //   );
+  }
+
+}
